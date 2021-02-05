4bebab81495bfabc0aa73ddffa4b01906b64c6fe
Ravi Mashru <mashru.ravi@gmail.com>
2021-02-05 23:03:44 +0530

    redirect to details page after creating new task

--- app/containers/TaskDetails/saga.js
+++ app/containers/TaskDetails/saga.js
@@ -1,8 +1,9 @@
-import { takeEvery, put, call } from 'redux-saga/effects';
-
+import { takeEvery, put, call, select } from 'redux-saga/effects';
+import { push } from 'connected-react-router';
 import request from 'utils/request';
+import { makeSelectLocation } from 'containers/App/selectors';
 
-import { ADD_TASK } from './constants';
+import { ADD_TASK, ADD_TASK_SUCCESS } from './constants';
 import { addTaskError, addTaskSuccess } from './actions';
 
 export function* addTask({ payload }) {
@@ -21,8 +22,19 @@ export function* addTask({ payload }) {
   }
 }
 
+export function* addTaskSuccessRoute({ payload }) {
+  const { _id: taskId } = payload.task;
+
+  // Get the current location in the redux state
+  // and replace "new" with the task ID
+  const { pathname } = yield select(makeSelectLocation());
+  const newUrl = pathname.replace('new', taskId);
+  yield put(push(newUrl));
+}
+
 // Individual exports for testing
 export default function* taskDetailsSaga() {
   // See example in containers/HomePage/saga.js
   yield takeEvery(ADD_TASK, addTask);
+  yield takeEvery(ADD_TASK_SUCCESS, addTaskSuccessRoute);
 }
